4bf509bdd550f70ae1b669aca78557a8e95ee7d1
anadykhan <aiyoun.khan@gmail.com>
2024-12-13 10:27:16 +0600

    Home filters done

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,39 @@
+import Filter from "@/components/shared/Filter";
+import HomeFilters from "@/components/shared/home/HomeFilters";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
+import Link from "next/link";
+
 const Home = () => {
   return (
-    <div>
-    </div>
-  )
-}
-export default Home
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <div className="flex justify-end max-sm:w-full">
+          <Link href="/ask-question">
+            <Button className="primary-gradient !text-light-900 min-h-[40px] rounded-xl px-4 py-3">
+              Ask a question
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchBar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+      <HomeFilters />
+    </>
+  );
+};
+export default Home;
