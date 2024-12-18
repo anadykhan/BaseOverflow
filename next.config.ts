ea55e80039f59c51a3a56d87e7e21077bdb84619
anadykhan <aiyoun.khan@gmail.com>
2024-12-18 17:20:12 +0600

    Ignore typescript build errors turned on

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Disable ESLint checks during Vercel builds
   },
+  typescript: {
+    ignoreBuildErrors: true, // Disable TypeScript type checks during Vercel builds
+  },
   // Add other config options here if needed
 };
 
