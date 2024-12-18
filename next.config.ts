ef42ad5917cfd9cc9ab6f77b9a2105d2beb5330e
anadykhan <aiyoun.khan@gmail.com>
2024-12-18 17:16:20 +0600

    Ignore eslint during build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint checks during Vercel builds
+  },
+  // Add other config options here if needed
 };
 
 export default nextConfig;
